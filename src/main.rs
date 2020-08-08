b21e4a9b14db7b6eb97980980f5171b39fa7fbc2
hsiaofongw <hsiaofong.w@gmail.com>
2020-08-09 02:55:24 +0800

    update graphviz file

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,13 @@
-struct NodeValue {
+struct Node {
     value: i32,
     min: i32,
-    max: i32,
-    left: Node,
-    right: Node
-}
-
-enum Node {
-    Value(Box<NodeValue>),
-    Nil
-}
-
-use crate::Node::{Value, Nil};
-
-impl NodeValue {
-    fn to_graphviz(& self) -> String {
-
-        let value = format!("{{ <f0> node | {} }}", self.value);
-        let min = format!("{{ <f1> min | {} }}", self.min);
-        let max = format!("{{ <f2> max | {} }}", self.max);
-        let left = "{ <f3> left | <f4> ptr }".to_string();
-        let right = "{ <f5> right | <f6> ptr}".to_string();
-
-        let graphviz = format!(
-            "{{ {} | {} | {} | {} | {} }}",
-            value,
-            min,
-            max,
-            left,
-            right
-        );
-
-        return graphviz;
-    }
-}
-
-impl NodeValue {
-    pub fn new(value: i32, min: i32, max: i32) -> Self {
-        return NodeValue {
-            value: value,
-            min: min,
-            max: max,
-            left: Nil,
-            right: Nil
-        };
-    }
+    max: i32
 }
 
 fn main() {
 
     let mut input: Vec<i32> = vec![1, 0, -1, 0, -2, 2];
-    input.sort();
+    input.sort(); // After sorting: [-2, -1, 0, 0, 1, 2]
 
     let target: i32 = 0;
     let k_value: i32 = 4;
@@ -70,33 +27,5 @@ fn main() {
     }
 
     println!("Target is: {}", target);
-
-    let mut index: u32 = 0;
-    for item in input.iter() {
-        println!("Considering: {}th item: {}", index, item);
-
-        let mut max_if_we_take_it: i32 = 0;
-        let mut min_if_we_take_it: i32 = 0;
-        let mut max_if_we_dont_take_it: i32 = 0;
-        let mut min_if_we_dont_take_it: i32 = 0;
-
-        println!(
-            "If we take this item, then, \
-            \nthe minimum value of the collection could be: {}, \
-            \nthe maximum value of the collection could be: {}",
-            min_if_we_take_it,
-            max_if_we_take_it
-        );
-
-        println!(
-            "If we dont take this item, then, \
-            \nthe minimum value of the collection could be: {}, \
-            \nthe maximum value of the collection could be: {}",
-            min_if_we_dont_take_it,
-            max_if_we_dont_take_it
-        );
-
-        index = index + 1;
-    }
 
 }
